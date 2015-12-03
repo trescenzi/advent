9a88bfc3a084ccec03809b1423a7cfacddf02663
Thomas <trescenzi@gmail.com>
2015-12-03 18:02:23 -0500

    day1 is complete

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 use std::io::Read;
+use std::iter::Scan;
+use std::str::Chars;
 
 fn interpret_direction(curr_floor: &mut i32, direction: char) -> Option<i32> {
     *curr_floor += match direction {
@@ -11,16 +13,31 @@ fn interpret_direction(curr_floor: &mut i32, direction: char) -> Option<i32> {
     return Some(*curr_floor);
 }
 
-fn scan_directions(directions: String) -> Box<Iterator<Item=i32>> {
-    return Box::new(directions.chars()
-        .scan(0, interpret_direction));
+type SDirections<'a> = Scan<Chars<'a>, i32, fn(&mut i32, char) -> Option<i32>>;
+
+fn scan_directions<'a>(directions: &'a String) -> SDirections<'a> {
+    return directions.chars().scan(0, interpret_direction);
+}
+
+fn read_directions_file(file_name: &'static str) -> String {
+    let mut directions = String::new();
+    match File::open(file_name) {
+        Ok(mut f) => f.read_to_string(&mut directions).ok().expect("file read failed"),
+        Err(e) => {
+            println!("{}", e);
+            return "File read failed".to_string();
+        }
+    };
+    directions
 }
 
 pub fn get_floor_num() -> i32 {
-    let mut directions = String::new();
-    match File::open("src/directions.txt") {
-        Ok(mut f) => f.read_to_string(&mut directions).ok().expect("string failed"),
-        Err(_) => return -42
-    };
-    return scan_directions(directions).last().unwrap();
+    scan_directions(&read_directions_file("src/directions.txt")).last().unwrap()
+}
+
+pub fn first_basement_visit() -> u32 {
+    let position: usize = scan_directions(&read_directions_file("src/directions.txt"))
+        .position(|curr_floor| curr_floor == -1)
+        .unwrap();
+    position as u32
 }
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,5 @@ use advent::day1;
 
 fn main() {
     println!("Santa has to go to floor: {}", day1::get_floor_num());
+    println!("Santa gets to the basement first on floor: {}", day1::first_basement_visit());
 }
