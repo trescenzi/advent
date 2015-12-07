30664f62774808cad9b480f777f5899fd461d6fe
Thomas <trescenzi@gmail.com>
2015-12-06 19:47:55 -0500

    middle of day 6 finishing up a few things

---
+++ src/day6.rs
@@ -0,0 +1,73 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+fn parse_nums(nums: &str) -> Vec<usize> {
+    nums.split(',').map(|num| {
+        return num.trim().parse()
+                .ok()
+                .expect("Non-integer specified as a position")
+    }).collect()
+}
+
+pub fn parse_instruction(instruction : String) -> (String, usize, usize, usize, usize) {
+    let split_instructions : Vec<&str> = instruction.split_whitespace().collect();
+    if split_instructions[0] == "toggle".to_string() {
+        let start : Vec<usize> = parse_nums(split_instructions[1]);
+        let end : Vec<usize> = parse_nums(split_instructions[3]);
+        return (split_instructions[0].to_string(), start[0], start[1], end[0], end[1]);
+    } else {
+        let start : Vec<usize> = parse_nums(split_instructions[2]);
+        let end : Vec<usize> = parse_nums(split_instructions[4]);
+        return (split_instructions[1].to_string(), start[0], start[1], end[0], end[1]);
+    }
+}
+
+pub fn lights_left_on() -> usize {
+    let mut lights : [[u32; 1000]; 1000] = [[0; 1000]; 1000];
+    for line in BufReader::new(File::open("src/light_instructions.txt").ok().expect("Bad file_name")).lines() {
+        let (instruction, start_x, start_y, end_x, end_y) = parse_instruction(line.ok().expect("Bad line"));
+        for y in start_y..end_y + 1 {
+            for x in start_x..end_x + 1 {
+                lights[x][y] = 
+                    if instruction == "on" {
+                        1
+                    } else if instruction == "off" {
+                        0
+                    } else {
+                        if lights[x][y] == 1 {
+                            0
+                        } else {
+                            1
+                        }
+                    };
+            }
+        }
+    }
+    lights.into_iter().map(|row| row.into_iter().filter(|x| **x == 1).count())
+        .fold(0, |sum, curr| sum + curr)
+}
+
+pub fn brightness() -> usize {
+    let mut lights : [[u32; 1000]; 1000] = [[0; 1000]; 1000];
+    for line in BufReader::new(File::open("src/light_instructions.txt").ok().expect("Bad file_name")).lines() {
+        let (instruction, start_x, start_y, end_x, end_y) = parse_instruction(line.ok().expect("Bad line"));
+        for y in start_y..end_y + 1 {
+            for x in start_x..end_x + 1 {
+                lights[x][y] +=
+                    if instruction == "on" {
+                        1
+                    } else if instruction == "off" {
+                        -1
+                    } else {
+                        2
+                    };
+                if lights[x][y] < 0 {
+                    lights[x][y] = 0;
+                }
+            }
+        }
+    }
+    lights.into_iter().map(|row| row.into_iter().fold(0, |sum : u32, curr| sum + curr))
+        .fold(0, |sum, curr| sum + curr)
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate advent;
 use advent::day1;
 use advent::day2;
 use advent::day5;
+use advent::day6;
 
 fn main() {
     println!("Santa has to go to floor: {}", day1::get_floor_num());
@@ -11,4 +12,6 @@ fn main() {
     println!("Present bow_length_required: {}", day2::ribbon_needed());
     println!("Number of nice texts: {}", day5::num_nice_texts());
     println!("Number of nice texts take two: {}", day5::num_nice_texts_take_two());
+    println!("Number of lights left on {}", day6::lights_left_on());
+    println!("Number of lights left on {}", day6::brightness());
 }
